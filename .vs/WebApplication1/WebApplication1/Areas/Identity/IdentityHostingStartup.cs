9c905aeb62001306791f75ef68cc6e655769b7cf
Harpreet Kaur <harpreetkaur11379@gmail.com>
2022-03-31 18:22:15 +0530

    Adding New Scaffolding items, identity
    Adding New Scaffolding items, identity  and override all files and data context class

---
+++ .vs/WebApplication1/WebApplication1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RegisterLoginProject.Data;
+
+[assembly: HostingStartup(typeof(RegisterLoginProject.Areas.Identity.IdentityHostingStartup))]
+namespace RegisterLoginProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<RegisterLoginProjectContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("RegisterLoginProjectContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<RegisterLoginProjectContext>();
+            });
+        }
+    }
+}
